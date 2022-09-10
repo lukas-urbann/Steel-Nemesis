e2939186a26d047e336c8ade30f143b57cf6c328
Lukáš Urban <lukas.urbann@seznam.cz>
2022-09-10 20:15:09 +0200

    half enemy update

--- Steel Nemesis Game/Assets/Skripty/Nove/Enemy/BasicEnemy.cs
+++ Steel Nemesis Game/Assets/Skripty/Nove/Enemy/BasicEnemy.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Controllers;
 using Other;
 using Triggers;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Enemy
 {
@@ -28,24 +26,16 @@ namespace Enemy
     }
 
    public class BasicEnemy : MonoBehaviour
-   {
-       public GameObject barrierCollectible,
-           damageCollectible,
-           speedCollectible,
-           firepowerCollectible,
-           cooldownCollectible,
-           hitpointsCollectible,
-           energyCollectible;
-       
-       protected float speed;
+    {
+        protected float speed;
         protected float hp;
         protected float maxHp;
         protected float damage;
-        protected bool isColliding = false;
-        protected int minWave, maxWave;
 
         protected FlyDirection flyDirection;
         public GameObject hitEffect;
+        
+        protected Vector3 movement = new Vector3(0, 0,0);
 
         [SerializeField] protected GameObject laser;
         [SerializeField] protected List<Transform> barrelPoints = new List<Transform>();
@@ -68,12 +58,38 @@ namespace Enemy
             speed += spd;
         }
 
-        protected void ChangeDirection(FlyDirection side)
+        protected virtual void ChangeDirection(FlyDirection side)
+        {
+            switch (side)
+            {
+                case FlyDirection.Down:
+                    movement = new Vector2(0, -speed);
+                    break;
+                case FlyDirection.Left:
+                    movement = new Vector2(-speed, 0);
+                    break;
+                case FlyDirection.Right:
+                    movement = new Vector2(speed, 0);
+                    break;
+                case FlyDirection.Top:
+                    movement = new Vector2(0, speed);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected virtual void CalculateFire()
         {
             
-        } 
+        }
 
         protected void Fire()
+        {
+            
+        }
+
+        private void Update()
         {
             
         }
@@ -109,7 +125,6 @@ namespace Enemy
                 Controllers.Game.Instance.AddKill();
                 Controllers.Wave.Instance.SetRemainingEnemies(-1);
                 CustomDeath();
-                DropConsumable();
                 deathScript.DeathEvent();
             }
         }
@@ -119,44 +134,8 @@ namespace Enemy
             Controllers.Score.Instance.AddScore((int) ((speed + maxHp/3 + damage) * Controllers.Wave.Instance.GetLevel()) );  
         }
 
-        protected void DropConsumable()
-        {
-            int[] values = {0,0,1,0,4,0,0,3,0,6,0,0,7,0,0,5,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-            int selected = values[Random.Range( 0, values.Length )];
-
-            switch (selected)
-            {
-                case 1:
-                    Instantiate(damageCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 2:
-                    Instantiate(firepowerCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 3:
-                    Instantiate(hitpointsCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 4:
-                    Instantiate(cooldownCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 5:
-                    Instantiate(energyCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 6:
-                    Instantiate(speedCollectible, transform.position, Quaternion.identity);
-                    break;
-                case 7:
-                    Instantiate(barrierCollectible, transform.position, Quaternion.identity);
-                    break;
-                default:
-                    break;
-            }
-        }
-
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if(isColliding) return;
-            isColliding = true;
-            
             if (col.CompareTag("Trigger"))
             {
                 TypeOfTrigger trigger;
@@ -177,9 +156,8 @@ namespace Enemy
                         switch (col.gameObject.layer)
                         {
                             case 3:
-                                Barrier.Instance.ChangeHitpoints(-1);
-                                Controllers.Wave.Instance.SetRemainingEnemies(-1);
-                                deathScript.DeathEvent();
+                                Debug.Log("Konec hry");
+                                Controllers.Game.Instance.GameOver();
                                 break;
                             case 10:
                                 GetComponent<Boundaries>().enabled = false;
@@ -188,14 +166,6 @@ namespace Enemy
                         break;
                 }
             }
-            
-            StartCoroutine(Reset());
-        }
-        
-        IEnumerator Reset()
-        {
-            yield return new WaitForEndOfFrame();
-            isColliding = false;
         }
     }
 }
